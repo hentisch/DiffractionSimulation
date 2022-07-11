88788e9f482d0c773ee8953b85d96f9048b3d495
Henry Tischler <henry@pop-os.localdomain>
2022-07-11 10:42:23 -0600

    Write method to get points of simple cubic unit cell

---
+++ unit_cells.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+""" This file contains several python functions to return a set of coordinates represnting a single form of unit cell
+for use in a crystal lattice. This is returned as a 2d numpy matrix, with each element containg the (x, y, z) points 
+of each particular atom """
+
+def simple_cubic(side_length):
+    points = np.zeros((8, 3))
+
+    point_counter = 0
+    for x in (0, side_length):
+        for y in (0, side_length):
+            for z in (0, side_length):
+                print(x, y, z)
+                points[point_counter] = np.array([x, y, z])
+                point_counter += 1
+
+    return points
+
+print(sorted(simple_cubic(1), key=lambda x: x[-1]))
