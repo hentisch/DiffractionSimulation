3173e724374c9bee5114fdcad588ed035976db7d
Henry Tischler <henrygtischler@gmail.com>
2022-07-12 13:25:19 -0600

    fixed bug with midpoint formula

--- utils.py
+++ utils.py
@@ -1,2 +1,4 @@
 def midpoint_formula(point_a:tuple[int, int], point_b:tuple[int, int]) -> tuple[int, int]:
-    return (point_a[0] + point_a[1])/2, (point_b[0], point_b[1])/2
+    a = (point_a[0] + point_a[1])
+    b = (point_b[0] + point_b[1])
+    return a/2, b/2
