c765e73d37a0497bf1906a44c6e069e84435e82a
Henry Tischler <henrygtischler@gmail.com>
2022-07-13 09:40:30 -0600

    add index swap function to utils.py

--- utils.py
+++ utils.py
@@ -9,3 +9,8 @@ def shift_points(points:np.array, x_shift:float, y_shift:float, z_shift:float):
     points[:,0] += x_shift
     points[:,1] += y_shift
     points[:,2] += z_shift
+
+def swap_indices(arr:np.array, index_a:int, index_b:int):
+    temp = arr[index_a]
+    arr[index_a] = arr[index_b]
+    arr[index_b] = temp
