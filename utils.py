e7ec7c90cc692321b167f8eb9f78cefabb61ea0b
Henry Tischler <henrygtischler@gmail.com>
2022-07-13 15:34:45 -0600

    add misc utils

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import itertools
 import numpy as np
+import copy
 
 def midpoint_formula(point_a:tuple[int, int], point_b:tuple[int, int]) -> tuple[int, int]:
     a = (point_a[0] + point_a[1])
@@ -11,6 +13,13 @@ def shift_points(points:np.array, x_shift:float, y_shift:float, z_shift:float):
     points[:,2] += z_shift
 
 def swap_indices(arr:np.array, index_a:int, index_b:int):
-    temp = arr[index_a]
+    temp = copy.copy(arr[index_a])
     arr[index_a] = arr[index_b]
     arr[index_b] = temp
+
+def flatten_matrix(matrix:list):
+    items = []
+    for row in matrix:
+        for element in row:
+            items.append(element)
+    return items
