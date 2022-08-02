79d0f32ad4de2245730dd53d90acda29503b0000
Henry Tischler <henrygtischler@gmail.com>
2022-08-01 20:16:26 -0600

    add script to generate plots for slideshow

---
+++ generate_plots.py
@@ -0,0 +1,30 @@
+from wave_interference import *
+import subprocess
+
+def pi_shifted_wave():
+    w = ComponentWave(1, 0)
+    a = ComponentWave(1, np.pi)
+    w_a = w+a
+
+    n = WavePlot([w, a], w_a, 2, 10000)
+
+    n.plot("π Shifted Waves", "materials/slideshow/plots/pi_shifted_matplotlib.png", 7/9)
+
+def in_phase_wave():
+    w = ComponentWave(1, 0)
+    a = ComponentWave(1, 0)
+    w_a = w+a
+
+    n = WavePlot([w, a], w_a, 2, 10000)
+    n.plot("In Phase Waves", "materials/slideshow/plots/in_phase_matplotlib.png", 7/9)
+
+def download_wave_interference_image():
+    subprocess.run(["wget", "https://upload.wikimedia.org/wikipedia/commons/d/d5/Two_point_source_interference_Pattern_-_panoramio.jpg", "-O", "materials/slideshow/images/PondWaveInterference.jpg"])
+
+def main():
+    pi_shifted_wave()
+    in_phase_wave()
+    download_wave_interference_image()
+
+if __name__ == "__main__":
+    main()
