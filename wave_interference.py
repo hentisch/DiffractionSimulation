b932f7d96ffd379e5156f47925c4eb5ada069574
Henry Tischler <henrygtischler@gmail.com>
2022-08-01 20:16:41 -0600

    update wave interference plot to support different aspect ratios

--- wave_interference.py
+++ wave_interference.py
@@ -1,3 +1,4 @@
+from statistics import mean
 import wave
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,7 +7,10 @@ import utils as u
 
 import wave_conversions as wc
 
+import string
+
 class Wave:
+
     def plot(self, num_samples, color, max, min=0):
         u.add_graph_to_plot(self, min, max, num_samples=num_samples, color=color)
     
@@ -15,13 +19,6 @@ class Wave:
 
         return [self(x) for x in x_values]
 
-
-class ComponentWave(Wave):
-    def __init__(self, wavelength:float, added_phase:float) -> None:
-        self.set_wavelength(wavelength)
-
-        self.added_phase = added_phase
-    
     def set_wavelength(self, wavelength:float):
         self.wavelength = wavelength
 
@@ -29,8 +26,16 @@ class ComponentWave(Wave):
         self.angular_freq = wc.convert(wavelength, "wavelength", "angular_frequency")
         self.time_period = wc.convert(wavelength, "wavelength", "time_period")
 
+class ComponentWave(Wave):
+    def __init__(self, wavelength:float, added_phase:float, amplitude:float=1) -> None:
+        self.set_wavelength(wavelength)
+
+        self.added_phase = added_phase
+        
+        self.amplitude = 1
+
     def __call__(self, time:float):
-        return np.exp(1j * (self.wavenumber*time+self.added_phase - self.angular_freq*self.time_period))
+        return np.cos(self.wavenumber*time+self.added_phase - self.angular_freq*self.time_period) * self.amplitude
     
     def __add__(self, other:Wave):
         return CompositeWave([self, other])
@@ -38,6 +43,8 @@ class ComponentWave(Wave):
 class CompositeWave(Wave):
     def __init__(self, waves:list[ComponentWave]) -> None:
         self.waves = list(waves)
+        self.wavelength = mean([wave.wavelength for wave in self.waves])
+        self.amplitude = mean([wave.amplitude for wave in self.waves])
     
     def __call__(self, time):
         value = 0
@@ -75,35 +82,34 @@ class WavePlot:
 
         self.wave_lines = []
     
-    def plot(self):
+    def plot(self, title:str="Wave Interference", filepath:str=None, aspect_ratio:float=4/3, inches_per_unit:float=1):
         fig, ax = plt.subplots()
+
+        x_left, x_right = ax.get_xlim()
+        y_low, y_high = ax.get_ylim()
+        ax.set_aspect(abs((x_right-x_left)/(y_low-y_high))*aspect_ratio)
+
         for i, wave in enumerate(self.all_waves):
             x_values = np.linspace(0, self.max, self.num_samples)
             y_values = [wave(x) for x in x_values]
-            self.wave_lines.append(plt.plot(x_values, y_values, color=self.colors[i%len(self.colors)]))
+            if i == len(self.all_waves)-1: #This is true when we are on the composite wave:
+                # label = f"Composite Wave, Wavelength - {wave.wavelength}, Amplitude - {wave.amplitude}"
+                label = "Composite Wave"
+            else:
+                label = f"Wavelength - {wave.wavelength}, Amplitude - {wave.amplitude}" 
+            self.wave_lines.append(plt.plot(x_values, y_values, color=self.colors[i%len(self.colors)], label=label))
 
-        plt.subplots_adjust(left=0.5, bottom=0.5)
+        plt.legend(loc="upper left")
 
-        # wave_a_shift = plt.axes([0.1, 0.1, 0.03, 1])
-        wave_a_shift = plt.axes([0.25, 0.2, 0.65, 0.03])
-        wave_a_shift_slider = plt.Slider(
-            ax=wave_a_shift, 
-            label="Wave A Phase shift",
-            valmin=0,
-            valmax=2,
-            valinit=0
-        )
+        plt.xlabel("Time")
+        plt.ylabel("Field Strength")
+        plt.title(title)
 
-        wave_a_wavelength = plt.axes([0.25, 0.1, 0.65, 0.03])
-        wave_a_wavelength_slider = plt.Slider(
-            ax=wave_a_wavelength,
-            label="Wave A Wavelength",
-            valmin=0,
-            valmax=10,
-            valinit=0
-        )
-        wave_a_wavelength_slider.on_changed(lambda x: self.update_wavelength(self.component_waves[0], wave_a_shift_slider.val))
-        plt.show()
+        if filepath == None:
+            plt.show()
+        else:
+            plt.savefig(filepath, dpi=300)
+
 
     def update_wavelength(self, wave, new_wavelength:float, fig):
         wave.wavelength  = new_wavelength
@@ -118,12 +124,3 @@ class WavePlot:
         for i, line in enumerate(self.wave_lines):
             line.set_ydata(self.all_waves[i].get_y_values(x_values))
         fig.canvas.draw_idle()
-        
-
-w = ComponentWave(1, 0)
-a = ComponentWave(2, np.pi)
-w_a = w+a
-
-n = WavePlot([w, a], w_a, 10, 1000)
-
-n.plot()
