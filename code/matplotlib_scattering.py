6daf4dfe7ab9d626fdd266ba8c8b34be582016c6
Henry Tischler <mysterykeven@gmail.com>
2022-08-04 20:17:10 -0600

    add meshgrid plots to matplotlib scattering script

--- code/matplotlib_scattering.py
+++ code/matplotlib_scattering.py
@@ -3,11 +3,11 @@ import numpy as np
 from array_utils import fill_skipping
 
 from geometry_utils import index_by_dimension
-from thomson_scattering import scattering_by_space
+from thomson_scattering import scattering_by_angle
 
 from tqdm import tqdm
 
-def get_thomson_scattering_slice(side_length:float, num_samples:int, axis:str, slice_ind:int, point_of_electrion:tuple, point_of_observation:tuple, value="amplitude"):
+""" def get_thomson_scattering_slice(side_length:float, num_samples:int, axis:str, slice_ind:int, point_of_electrion:tuple, point_of_observation:tuple, value="amplitude"):
     coordinates = np.linspace(0, side_length, round(np.sqrt(num_samples)))
     x_grid, y_grid = np.meshgrid(coordinates, coordinates)
 
@@ -33,3 +33,18 @@ if __name__ == "__main__":
 
     get_thomson_scattering_slice(10, 10000, 'z', 0, (5, 0, 5), (5 ,-10, 5))
     plt.show()
+"""
+
+min1 = -0.1
+max1 = 0.1
+step = 0.001
+xPts = np.arange(min1, max1 + step, step)
+yPts = np.arange(min1, max1 + step, step)
+xx, yy = np.meshgrid(xPts, yPts)
+radius = np.sqrt(xx ** 2 + yy ** 2)
+angle = np.arctan(np.abs(yy / xx))
+
+amplitude, phase = scattering_by_angle(angle, radius, 0, 1, 1)
+plt.imshow(amplitude*radius, origin='lower')
+plt.colorbar()
+plt.show()
