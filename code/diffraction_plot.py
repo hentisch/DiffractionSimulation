2f796e6e596e530366f78393522baf92ed281fc8
Henry Tischler <henrygtischler@gmail.com>
2022-08-04 18:04:05 -0600

    update diffraction plot

--- code/diffraction_plot.py
+++ code/diffraction_plot.py
@@ -1,13 +1,32 @@
+import numpy as np
+from scipy import integrate
+
+from geometry_utils import angle_between_points, get_2d_points, get_3d_cos_wave, get_3d_cos_wave_between_points, rotate_2d_points
+from math import dist
 from crystal_latttice import CrystalLattice
 from unit_cell import UnitCell
 
 import matplotlib.pyplot as plt
 
-def plot_2d_lattice(lattice:CrystalLattice, axis:str, slice_ind:int):
-    points = lattice.get_2d_slice(axis, slice_ind)
+def get_2d_lattice_points(lattice:CrystalLattice, axis:str, slice_ind:int):
+    raw_points = lattice.get_2d_slice(axis, slice_ind)
 
-    plt.scatter(points[:,0], points[:,1])
-    plt.show()
+    return np.unique(raw_points, axis=0)
+
+class DiffractionPlot:
+    
+    def __init__(self) -> None:
+        self.points = get_2d_lattice_points(CrystalLattice((3, 1, 1), UnitCell.simple_cubic(1)), 'y', 1)
+    
+    def plot_points(self):
+        plt.scatter(self.points[:,0], self.points[:,1])
+        plt.show()
+    
+    def calculate_diffraction(self, point_of_observation:tuple, angle_of_scattering:float):
+        for point in self.points:
+            atomic_distance = dist(point, point_of_observation)
+            scattering_value = s
 
 if __name__ == "__main__":
-    plot_2d_lattice(CrystalLattice((4, 1, 3), UnitCell.simple_cubic(1)), 'y', 1)
+    plot = DiffractionPlot()
+    plot.plot_points()
